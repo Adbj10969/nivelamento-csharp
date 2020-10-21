66cb8a99e1a23329dc8f54f9b675fef08278db3e
Adbj10969 <43136459+Adbj10969@users.noreply.github.com>
2020-10-21 15:21:36 -0300

    Program.cs
    using System;
    
    namespace ExerciciosPropostos1_2_
    {
        class Program
        {
            static void Main(string[] args)
            {
                //
                double R, A, pi = 3.14159;
                Console.WriteLine("Digite o valor do raio do círculo: ");
                R = double.Parse(Console.ReadLine());
                A = pi * Math.Pow(R, 2);
                Console.WriteLine("O valor da área do circulo é equivalente a: ");
                Console.WriteLine("A {0}", A.ToString("F4"));
            }
        }
    }

--- uri1002/uri1002/Program.cs
+++ uri1002/uri1002/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Globalization;
+using System;
 
-namespace uri1002 {
-    class Program {
-        static void Main(string[] args) {
-
+namespace ExerciciosPropostos1_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
             double R, A, pi = 3.14159;
-
-            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            A = pi * R * R;
-
-            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Digite o valor do raio do círculo: ");
+            R = double.Parse(Console.ReadLine());
+            A = pi * Math.Pow(R, 2);
+            Console.WriteLine("O valor da área do circulo é equivalente a: ");
+            Console.WriteLine("A {0}", A.ToString("F4"));
         }
     }
 }
